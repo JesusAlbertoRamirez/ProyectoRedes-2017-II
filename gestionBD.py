68138849bb0650173f73ac7829d339a398522375
kairos-55 <jisus-55@hotmail.com>
2017-10-27 12:58:46 -0500

    Módulos del proyecto

--- gestionBD.py
+++ gestionBD.py
@@ -1,4 +1,19 @@
 """Este módulo se encarga de establecer la conexión con la base de datos 
 para crear un nuevo registro, modificar uno ya existente o consultarlo, 
 teniendo en cuenta que la intranet es la que puede crear y modificar registros, 
-mientras que el cliente es el encargado de hacer consultas."""
+mientras que el cliente es el encargado de hacer consultas.
+
+En esta base de datos existirán las siguientes entidades con los siguientes
+atributos:
+
+sistemaOpertativo: (kernel, release, nodo, version, maquina, procesador, so, hardware)
+
+usuario: (nombre)
+
+cpu: (noKernel, kernel, inactividad, esperaES, maquinaVirtual)
+
+memoria: (virtual, disponible, buffer, cache)
+
+memoriaIntercambio: (desdeDisco, enElDisco)
+
+"""
